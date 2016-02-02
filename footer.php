b8fe6a3db44fd4e6394f501380383277deab5e47
AGnjatic <aleksandar.gnjatic@live.com>
2016-02-02 22:00:58 +0100

    header and footer finished with slideshow added

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 </div>
+<footer>
 
+<div class="footertext">&copy; Developed by Aki Car</div>
 
-
-
-
+</footer>
 </body>
 <?php wp_footer(); ?>
 </html>
--- header.php
+++ header.php
@@ -4,4 +4,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+
+<header>
+  <div class="logo">
+    <a href="#"></a>
+  </div>
+  <div class="nav">
+    <div class="navicons">
+      <a href="https://www.facebook.com/"><img src="https://www.facebook.com/images/fb_icon_325x325.png"></a>
+      <a href="https://www.youtube.com/"><img src="http://squareonesask.ca/wp-content/uploads/2015/05/YouTube.png"></a>
+    </div>
+    <ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Projects</a></li>
+  <li><a href="#">Gallery</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+  </div>
+</header>
+
   <div class="main">
+<div class="slideshow">
+
+</div>
