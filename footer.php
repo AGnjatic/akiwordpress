82a1f0d93861625fbb577fea711a2e14c1ca4375
AGnjatic <aleksandar.gnjatic@live.com>
2016-02-02 17:57:06 +0100

    reseting all pages, removing bootstrap

--- footer.php
+++ footer.php
@@ -1,15 +1,9 @@
 </div>
-<div class = "navbar navbar-default navbar-fixed-bottom">
 
-			<div class = "container">
-				<p class = "navbar-text pull-left">Developed by Aki Car</p>
-			</div>
 
-		</div>
 
-		
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'></script>
-<script src= <?php echo get_template_directory_uri().'/js/bootstrap.min.js';?>></script>
+
+
 </body>
 <?php wp_footer(); ?>
 </html>
--- header.php
+++ header.php
@@ -4,42 +4,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-
-  <div class = "navbar navbar-inverse navbar-static-top">
-  			<div class = "container">
-
-  				<a href="#" class="pull-left"><img src=<?php echo get_template_directory_uri().'/img/logo.jpg'?> width="50%"></a>
-
-  				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
-  					<span class = "icon-bar"></span>
-  					<span class = "icon-bar"></span>
-  					<span class = "icon-bar"></span>
-  				</button>
-
-  				<div class = "collapse navbar-collapse navHeaderCollapse">
-
-  					<ul class = "nav navbar-nav navbar-right">
-
-  						<li><a href = "#">Home</a></li>
-
-  						<li class = "dropdown">
-
-  							<a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">Projects<b class = "caret"></b></a>
-  							<ul class = "dropdown-menu">
-  								<li><a href = "#">Project 1</a></li>
-  								<li><a href = "#">Project 2</a></li>
-  								<li><a href = "#">Project 3</a></li>
-  								<li><a href = "#">Project 4</a></li>
-  							</ul>
-
-  						</li>
-  						<li><a href = "#">Gallery</a></li>
-  						<li><a href = "#contact" data-toggle="modal">Contact</a></li>
-
-  					</ul>
-
-  				</div>
-
-  			</div>
-  		</div>
   <div class="main">
